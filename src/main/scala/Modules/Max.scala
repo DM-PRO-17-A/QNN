d6e4684c1dff5325efdf3d87b565905ba7b7e491
Christoffer Wilhelm Gran <cwgran@gmail.com>
2017-10-05 18:37:56 +0200

    Added Sum module And making Max much simpler and running in one cycle

--- src/main/scala/Modules/Max.scala
+++ src/main/scala/Modules/Max.scala
@@ -10,36 +10,15 @@ class Max(nums_in: Int, dataWidth: Int) extends Module {
 	val io = new Bundle {
 		val nums = Vec.fill(nums_in){UInt(INPUT, dataWidth)}
 
-		val dataValid = Bool(OUTPUT)
 		val dataOut = UInt(OUTPUT, dataWidth)
 	}
-	
-	val currentIndex = Reg(init=(UInt(0,dataWidth)))
-	val currentBiggest = Reg(init=(UInt(0,dataWidth)))
-	io.dataOut := currentBiggest
-	io.dataValid := Bool(false)
 
-	when(currentIndex === UInt(nums_in)){
-		io.dataValid := Bool(true)
-	} .otherwise {
-		currentIndex := currentIndex + UInt(1)
-	}
-
-	for(i <- 0 to nums_in-1){
-		when(currentIndex === UInt(i)) {
-			currentBiggest := Compare(currentBiggest, io.nums(i))
-		}
-	}
-
+	io.dataOut := io.nums.reduceLeft(Compare)
 }
 
 class MaxTests(c: Max) extends Tester(c) {
 	poke(c.io.nums, Array[BigInt](9,14,2,3))
-
-	for(i <- 0 to 4){
-		peek(c.io.dataValid)
-		peek(c.io.dataOut)
-		step(1)
-	}
+	peek(c.io.dataOut)
+	
 }
 
