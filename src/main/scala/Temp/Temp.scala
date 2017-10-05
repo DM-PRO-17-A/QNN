bcf80a7618b1aebb869d09813b8b097daad74d55
alfiehub <alfiehub@users.noreply.github.com>
2017-10-05 12:09:18 +0200

    Add Mux2 as an example

--- src/main/scala/Temp/Temp.scala
+++ src/main/scala/Temp/Temp.scala
@@ -1,9 +1,12 @@
 package Temp
 
 import Chisel._
+import Modules._
 
 object TempMain {
   def main(args: Array[String]) {
+    var cmdArgs = args.slice(1, args.length) 
     println("YAY!")
+    chiselMainTest(cmdArgs, () => Module(new Mux2())){ c => new Mux2Tests(c)}
   }
 }
