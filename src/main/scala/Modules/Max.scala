fafd1b63b92465997f4dd391d386a26685b9b2e9
alfiehub <alfiehub@users.noreply.github.com>
2017-10-05 19:00:06 +0200

    Replace tabs with spaces and use expect in Sum/Max tests

--- src/main/scala/Modules/Max.scala
+++ src/main/scala/Modules/Max.scala
@@ -3,22 +3,22 @@ package Modules
 import Chisel._
 
 class Max(nums_in: Int, dataWidth: Int) extends Module {
-	def Compare(a:UInt, b:UInt):UInt = {
-		Mux(a > b, a, b)
-	}
+  def Compare(a:UInt, b:UInt):UInt = {
+    Mux(a > b, a, b)
+  }
 
-	val io = new Bundle {
-		val nums = Vec.fill(nums_in){UInt(INPUT, dataWidth)}
+  val io = new Bundle {
+    val nums = Vec.fill(nums_in){UInt(INPUT, dataWidth)}
 
-		val dataOut = UInt(OUTPUT, dataWidth)
-	}
+    val dataOut = UInt(OUTPUT, dataWidth)
+  }
 
-	io.dataOut := io.nums.reduceLeft(Compare)
+  io.dataOut := io.nums.reduceLeft(Compare)
 }
 
 class MaxTests(c: Max) extends Tester(c) {
-	poke(c.io.nums, Array[BigInt](9,14,2,3))
-	peek(c.io.dataOut)
-	
+  val input = Array[BigInt](9,14,2,3)
+  poke(c.io.nums, input)
+  expect(c.io.dataOut, input.reduceLeft(_ max _))
 }
 
